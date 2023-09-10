47e77fd45b04545b536749fbd0080551dc3b9302
Jordan Sharpe <86000863+jsharpe27@users.noreply.github.com>
2023-09-10 17:06:30 -0400

    workin on it

--- src/components/Starter.jsx
+++ src/components/Starter.jsx
@@ -4,23 +4,26 @@ import {motion} from 'framer-motion'
 const Starter = ({handleStart}) => {
   return (
     
-    <motion.div className='flex items-center justify-center flex-col'
-    initial={{  x:-400}}
-    animate={{  x:0 }}
-    transition={{ type: 'spring', stiffness: 80, damping: 10, duration: 1 }}
+    <div className='flex items-center justify-center flex-col'
+    
     >
 
             <motion.h1 className='mb-4 mt-4 px-4 text-2xl 
             font-medium !leading-[1.5] sm:text-4xl'
-            initial={{  y:-100}}
-            animate={{  y:0 }}
-            transition={{ type: 'spring', stiffness: 80, damping: 10, duration: 1 }}
             >Build your Pokemon Team!</motion.h1>
      
-      <h2 className='text-2xl'>Choose a Starter pokemon</h2>
+      <motion.h2 className='text-2xl'
+      initial={{  x:-400}}
+      animate={{  x:0 }}
+      transition={{ type: 'spring', stiffness: 80, damping: 10, duration: 1 }}
+      >Choose a Starter pokemon</motion.h2>
 
-      <div className='flex bg-gray-300 mt-4 gap-[5rem]' >
-            <div>
+      <motion.div className='flex mt-4 gap-[5rem] max-[50rem] justify-center items-center' 
+      initial={{  x:-400}}
+      animate={{  x:0 }}
+      transition={{ type: 'spring', stiffness: 80, damping: 10, duration: 1 }}
+      >
+            <div className='flex flex-col '>
                   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" 
                   alt="sprite of Bulbasaur"
                   />
@@ -34,11 +37,16 @@ const Starter = ({handleStart}) => {
                         specialDefense={65}
                         speed={45}
                   />
-                  <button className='mb-4 mt-4 text-2xl' onClick={()=> handleStart(1)}>Choose Bulbasaur </button>
+                  <button className='group bg-green-300 text-black px-7
+                        py-3 flex items-center gap-2 rounded-full
+                        outline-none focus:scale-110 hover:scale-110
+                        hover:bg-emerald-900 hover:text-white active:scale-105
+                        transition' 
+                        onClick={()=> handleStart(1)}>Choose Bulbasaur </button>
             </div> 
 
-            <div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="sprite of Charmander" />
+            <div className='flex flex-col'>
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="sprite of Charmander" />
                   <h3 classNam>Charmander</h3>
                   <Stats
                         id={2}
@@ -50,11 +58,16 @@ const Starter = ({handleStart}) => {
                         speed={65}
                   />
                   
-                  <button onClick={()=> handleStart(4)}>Choose Charmander </button>
+                  <button className='group bg-red-300 text-black px-7
+                        py-3 flex items-center gap-2 rounded-full
+                        outline-none focus:scale-110 hover:scale-110
+                        hover:bg-red-900 hover:text-white active:scale-105
+                        transition' 
+                  onClick={()=> handleStart(4)}>Choose Charmander </button>
             </div> 
 
-            <div >
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="sprite of Squirtle" />
+            <div className='flex flex-col'>
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="sprite of Squirtle" />
                   <h3 classNam>Squirtle</h3>
                   <Stats
                         id={3}
@@ -65,10 +78,16 @@ const Starter = ({handleStart}) => {
                         specialDefense={64}
                         speed={43}
                   />
-                  <button onClick={()=> handleStart(7)}>Choose Squirtle </button>
+                  <button className='group bg-blue-300 text-black px-7
+                        py-3 flex items-center gap-2 rounded-full
+                        outline-none focus:scale-110 hover:scale-110
+                        hover:bg-blue-900 hover:text-white active:scale-105
+                        transition' 
+                  onClick={()=> handleStart(7)}>Choose Squirtle </button>
             </div>
+      
+      </motion.div>
       </div>
-    </motion.div>
   )
 }
 
